b51395eed9ea0245f68e3361bbd9bbe29fcba3c1
nathanlschneider <nlschneider@gmail.com>
2018-07-20 15:08:35 -0400

    added views

--- app.js
+++ app.js
@@ -2,5 +2,20 @@ var app = require('express')();
 
 app.set("view engine", "ejs");
 
+app.get("/", function (req, res) {
+    res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res) {
+    var campgrounds = [
+        {name:"Salmon Creen", image:""},
+        {name:"Granite Hill", image:""},
+        {name:"Mountain Goat's Rest", image:""}
+    ];
+    res.render("campgrounds");
+});
+
 app.listen(3000);
+
+
 
